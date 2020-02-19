1a58db60a3207abb962025d3b690ae130a75df11
izacklee <285152425@qq.com>
2020-02-19 16:33:05 +0800

    把if改为switch

--- src/test/java/com/zeroten/javales/day02y_io/homework/FileSystem.java
+++ src/test/java/com/zeroten/javales/day02y_io/homework/FileSystem.java
@@ -31,156 +31,145 @@ public class FileSystem {
             int[] numArr = {0, 1, 2, 3, 4, 5, 6};
             String[] list = f.list();
             File[] listFiles = f.listFiles();
-
-            // 1.查看目录
-            if (num == 1) {
-                getFileList(list,listFiles);
-                System.out.print("输入0返回主菜单：");
-                num = s.nextInt();
-            }
-
-            // 2.查看文件
-            if(num == 2) {
-                System.out.print("请输入要查看的文件名：");
-                String name = s.next();
-                if(name == null) {
-                    System.out.println("文件名为空，请重新输入");
-                    num = 2;
-                }
-                File f2 = fileExists(path, name);
-                InputStream input = null;
-                if(f2 != null) {
-                    try {
-                        input = new FileInputStream(f2);
-                        int len = input.available(); // 获取文件字节长度
-                        byte[] bs = new byte[len];
-                        int temp = -1;
-                        int index = 0;
-                        while((temp = input.read()) != -1) {
-                            bs[index] = (byte) temp;
-                            index++;
-                        }
-                        String str = new String(bs);
-                        System.out.println(str);
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        if(input != null) {
-                            try {
-                                input.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
+            switch(num) {
+                case 1: // 1.查看目录
+                    getFileList(list,listFiles);
+                    System.out.print("输入0返回主菜单：");
+                    num = s.nextInt();
+                    break;
+                case 2: // 2.查看文件
+                    System.out.print("请输入要查看的文件名：");
+                    String name = s.next();
+                    if(name == null) {
+                        System.out.println("文件名为空，请重新输入");
+                        num = 2;
+                    }
+                    File f2 = fileExists(path, name);
+                    InputStream input = null;
+                    if(f2 != null) {
+                        try {
+                            input = new FileInputStream(f2);
+                            int len = input.available(); // 获取文件字节长度
+                            byte[] bs = new byte[len];
+                            int temp = -1;
+                            int index = 0;
+                            while((temp = input.read()) != -1) {
+                                bs[index] = (byte) temp;
+                                index++;
+                            }
+                            String str = new String(bs);
+                            System.out.println(str);
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            if(input != null) {
+                                try {
+                                    input.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
                             }
                         }
-                    }
-                } else {
-                    System.out.print("文件不存在，查看失败");
-                    System.out.println();
-                }
-                System.out.print("输入0返回主菜单：");
-                num = s.nextInt();
-            }
-
-            // 3.添加目录
-            if(num == 3) {
-                System.out.print("请输入添加的目录名：");
-                String name = s.next();
-                if(name == null) {
-                    System.out.println("目录名为空，请重新输入");
-                    num = 3;
-                }
-                File f2 = notFileExists(path, name);
-                if(f2 != null) {
-                    boolean b = f2.mkdirs();
-                    if(b) {
-                        System.out.println("添加成功");
                     } else {
-                        System.out.println("添加失败");
+                        System.out.print("文件不存在，查看失败");
+                        System.out.println();
                     }
-                } else {
-                    System.out.println("目录已存在，不能重复添加");
-                    System.out.println();
-                }
-                System.out.print("输入0返回主菜单：");
-                num = s.nextInt();
-            }
-
-            // 4.添加文件
-            if(num == 4) {
-                System.out.print("请输入添加的文件名：");
-                String name = s.next();
-                if(name == null) {
-                    System.out.println("文件名为空，请重新输入");
-                    num = 4;
-                }
-                File f2 = notFileExists(path, name);
-                OutputStream out = null;
-                if(f2 != null) {
-                    try {
-                        boolean b = f2.createNewFile();
+                    System.out.print("输入0返回主菜单：");
+                    num = s.nextInt();
+                    break;
+                case 3: // 3.添加目录
+                    System.out.print("请输入添加的目录名：");
+                    String name3 = s.next();
+                    if(name3 == null) {
+                        System.out.println("目录名为空，请重新输入");
+                        num = 3;
+                    }
+                    File f3 = notFileExists(path, name3);
+                    if(f3 != null) {
+                        boolean b = f3.mkdirs();
                         if(b) {
-                            System.out.print("请输入要写入文件的内容：");
-                            String content = s.next();
-                            // 获取内容的字节数组
-                            byte[] bs = content.getBytes();
-                            out = new FileOutputStream(f2);
-                            for(byte c : bs) {
-                                out.write(c); // 将数据写入文件
-                            }
-                            out.flush();
-                            out.close();
-                            System.out.println("文件添加成功");
+                            System.out.println("添加成功");
                         } else {
-                            System.out.println("文件添加失败");
+                            System.out.println("添加失败");
                         }
-                    } catch(Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        if(out != null) {
-                            try {
+                    } else {
+                        System.out.println("目录已存在，不能重复添加");
+                        System.out.println();
+                    }
+                    System.out.print("输入0返回主菜单：");
+                    num = s.nextInt();
+                    break;
+                case 4: // 4.添加文件
+                    System.out.print("请输入添加的文件名：");
+                    String name4 = s.next();
+                    if(name4 == null) {
+                        System.out.println("文件名为空，请重新输入");
+                        num = 4;
+                    }
+                    File f4 = notFileExists(path, name4);
+                    OutputStream out = null;
+                    if(f4 != null) {
+                        try {
+                            boolean b = f4.createNewFile();
+                            if(b) {
+                                System.out.print("请输入要写入文件的内容：");
+                                String content = s.next();
+                                // 获取内容的字节数组
+                                byte[] bs = content.getBytes();
+                                out = new FileOutputStream(f4);
+                                for(byte c : bs) {
+                                    out.write(c); // 将数据写入文件
+                                }
+                                out.flush();
                                 out.close();
-                            } catch(Exception e) {
-                                e.printStackTrace();
+                                System.out.println("文件添加成功");
+                            } else {
+                                System.out.println("文件添加失败");
+                            }
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            if(out != null) {
+                                try {
+                                    out.close();
+                                } catch(Exception e) {
+                                    e.printStackTrace();
+                                }
                             }
                         }
-                    }
-                } else {
-                    System.out.println("文件已存在，请勿重复添加");
-                    System.out.println();
-                }
-                System.out.print("输入0返回主菜单：");
-                num = s.nextInt();
-            }
-
-            // 5.删除文件
-            if(num == 5) {
-                System.out.print("请输入要删除的文件/目录名：");
-                String name = s.next();
-                if(name == null) {
-                    System.out.println("文件/目录名为空，请重新输入");
-                    num = 5;
-                }
-                File f2 = fileExists(path, name);
-                if(f2 != null) {
-                    boolean b = f2.delete();
-                    if(b) {
-                        System.out.println("删除成功");
                     } else {
-                        System.out.println("删除失败");
+                        System.out.println("文件已存在，请勿重复添加");
+                        System.out.println();
                     }
-                } else {
-                    System.out.println("文件/目录不存在，删除失败");
-                    System.out.println();
-                }
-                System.out.print("输入0返回主菜单：");
-                num = s.nextInt();
+                    System.out.print("输入0返回主菜单：");
+                    num = s.nextInt();
+                    break;
+                case 5: // 5.删除文件
+                    System.out.print("请输入要删除的文件/目录名：");
+                    String name5 = s.next();
+                    if(name5 == null) {
+                        System.out.println("文件/目录名为空，请重新输入");
+                        num = 5;
+                    }
+                    File f5 = fileExists(path, name5);
+                    if(f5 != null) {
+                        boolean b = f5.delete();
+                        if(b) {
+                            System.out.println("删除成功");
+                        } else {
+                            System.out.println("删除失败");
+                        }
+                    } else {
+                        System.out.println("文件/目录不存在，删除失败");
+                        System.out.println();
+                    }
+                    System.out.print("输入0返回主菜单：");
+                    num = s.nextInt();
+                    break;
+                case 6: // 6.退出系统
+                    System.out.println("系统已退出，欢迎再次使用。");
+                    break;
             }
-
-            // 6.退出系统
-            if(num == 6) {
-                System.out.println("系统已退出，欢迎再次使用。");
-            }
-
             // 判断输入操作数字是否有误
             if(Arrays.binarySearch(numArr, num) < 0 || ynum == 0) {
                 System.out.println("输入有误，请重新输入");
