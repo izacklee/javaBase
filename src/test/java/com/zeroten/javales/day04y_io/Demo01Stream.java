f342aa6366f2b4a1156aded1e9c59278a3221972
izacklee <285152425@qq.com>
2020-02-20 16:51:16 +0800

    阅读I/O流的源码

--- src/test/java/com/zeroten/javales/day04y_io/Demo01Stream.java
+++ src/test/java/com/zeroten/javales/day04y_io/Demo01Stream.java
@@ -1,8 +1,6 @@
 package com.zeroten.javales.day04y_io;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 public class Demo01Stream {
 
@@ -11,5 +9,22 @@ public class Demo01Stream {
         InputStream in1;
         FileInputStream in2;
         BufferedInputStream in3;
+        DataInputStream in4;
+        ByteArrayInputStream in5;
+
+        // 字符流
+        StringReader in6;
+        InputStreamReader in7;
+        BufferedReader in8;
+
+        // 字节流
+        OutputStream out1;
+        FileOutputStream out2;
+        BufferedOutputStream out3;
+        DataOutputStream out4;
+        PrintStream out5;
+
+        BufferedWriter out6;
+        OutputStreamWriter out7;
     }
 }
